00b67c0fbba3eed9e23eff1f71c608ecb25825c3
TAVARES-VARELA Arlindo <Arlindo.TAVARES-VARELA@cpnv.ch>
2019-12-17 14:16:24 +0100

    Création du projet

---
+++ main.c
@@ -0,0 +1,16 @@
+/*
+* \title main.c
+*\brief utilisation de sizeof
+* \ author Arlindo Tavares
+* \version 1.0
+* \date 03.09.2019
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
